91bb3d5da7354ec561d228325e478907b3203433
MCorange99 <gvidasjuknevicius2@gmail.com>
2022-06-22 22:41:27 +0300

    register !!!not working!!!

---
+++ src/api/routes/v1/user/POST/register.js
@@ -0,0 +1,70 @@
+const Util = require("@util/util");
+const { color } = require("@util/util")
+const Validation = require("../../../../util/validation");
+const pkgValidator = require("validator");
+const UserSchema = require("../../../../models/UserSchema");
+const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+    console.log(color("&r&2[&3users&2]&1|&2[&6POST&2] &1- &2postUser has been trigered"));
+    
+    const { username, email, displayName, password } = req.body;
+    if (!email || !username || !displayName || !password) {
+        return res.status(400).json({
+            code: "MissingProps",
+            status: 400,
+            props: ["email", "username", "displayName", "password"],
+            message: "email, username, displayName & password are necessary fields"
+        });
+    };
+
+    if (!(await Validation.isUsernameAvailable(username))) {
+        return res.status(409).json({
+            code: "UsernameAlreadyTaken",
+            status: 409,
+            message: "The username is already taken"
+        });
+    };
+
+    if (!pkgValidator.isEmail(email)) {
+        return res.status(400).json({
+            code: "InvalidEmail",
+            status: 400,
+            message: "Email address is not valid"
+        });
+    };
+
+    if (!(await Validation.isEmailAvailable(email))) {
+        return res.status(409).json({
+            code: "EmailAlreadyUsed",
+            status: 409,
+            message: "The email address is already in use"
+        });
+    };
+
+
+    const hash = Util.cyrb53(password).toString();
+
+    const _id = Util.generateID("user");
+    await new UserSchema({
+        _id, email, username, displayName, hash,
+        api: {
+            token: Util.generateTempToken()
+        }}
+        ).save();
+
+    let user = await UserSchema.findOne({ _id });
+    user = {
+        id: String(user._id),
+        username: String(user.username),
+        displayName: String(user.displayName),
+        hash: String(user.hash),
+        verified: Boolean(user.verified),
+        token: String(user.api.token),
+        createdAt: String(new Date(user.createdAt).toISOString())
+    };
+
+    return res.status(200).json({ ...user });
+});
+
+module.exports = router;
---
+++ src/util/utils.js
@@ -0,0 +1,129 @@
+const GuildSchema = require("@model/GuildSchema");
+const UserSchema = require("@model/UserSchema");
+module.exports = class Util extends null {
+    static generateID (type) {
+
+        let token = "";
+        switch (type){
+            case "channel":
+                do{
+                token = "C_";
+                const chars = "0123456789".split("");
+                for (let i = 0; i < 16; i++) {
+                    token += chars[Math.floor(Math.random() * chars.length)];
+                };
+
+                
+                } while (false)
+                return token;
+                break;
+            case "message":
+                do{
+                    token = "M_";
+                    const chars = "0123456789".split("");
+                    for (let i = 0; i < 16; i++) {
+                        token += chars[Math.floor(Math.random() * chars.length)];
+                    };
+    
+                    
+                    } while (false)
+                    return token;
+                    break;
+            case "guild":
+                do{
+                    token = "G_";
+                    const chars = "0123456789".split("");
+                    for (let i = 0; i < 16; i++) {
+                        token += chars[Math.floor(Math.random() * chars.length)];
+                    };
+    
+                    
+                    } while (!GuildSchema.findOne({token}))
+                    return token;
+                    break;
+            case "user":
+                /**
+                 * Thanks to MCorange
+                 * https://github.com/MCorange99
+                 */
+                let date = new Date();
+                let month = `${("0" + (date.getMonth() + 1)).slice(-2)}`;
+                let day = `${("0" + date.getDate()).slice(-2)}`;
+
+                let id = `U_${date.getFullYear()}${month}${day}${date.getHours()}${date.getMinutes()}${date.getSeconds()}${this.getRandomInt(10)}${this.getRandomInt(10)}${this.getRandomInt(10)}`;
+
+                return id;
+                break;
+            }
+    };
+
+    static generateTempToken () {
+        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        let token = "H_";
+
+        for (let i = 0; i < 32; i++) {
+            token += chars[Math.floor(Math.random() * chars.length)];
+        };
+
+        return token;
+    };
+
+    /**
+     * @param {number} max 
+     */
+    static getRandomInt (max) {
+        return Math.floor(Math.random() * (max + 1));
+    };
+
+
+    /**
+     * Found from https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+     * @param {*} str 
+     * @param {*} seed 
+     * @returns 
+     *///            DO NOT CHANGE SEED 
+    static cyrb53(str, seed = 0) {
+        let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+        for (let i = 0, ch; i < str.length; i++) {
+            ch = str.charCodeAt(i);
+            h1 = Math.imul(h1 ^ ch, 2654435761);
+            h2 = Math.imul(h2 ^ ch, 1597334677);
+        }
+        h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+        h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+        return 4294967296 * (2097151 & h2) + (h1>>>0);
+    };
+
+    /**
+     * 
+     * @param {*} str 
+     * @returns
+     */
+    static color(str){
+        str = str
+        .replace(/`?\&r`?/g,  "\x1b[0m")
+        .replace(/`?\&br`?/g, "\x1b[0m")
+        .replace(/`?\&dm`?/g, "\x1b[2m")
+        .replace(/`?\&u`?/g,  "\x1b[4m")
+        .replace(/`?\&bl`?/g, "\x1b[5m")
+        .replace(/`?\&rv`?/g, "\x1b[7m")
+        .replace(/`?\&hd`?/g, "\x1b[8m")
+        .replace(/`?\&0`?/g, "\x1b[30m")
+        .replace(/`?\&1`?/g, "\x1b[34m")
+        .replace(/`?\&2`?/g, "\x1b[32m")
+        .replace(/`?\&3`?/g, "\x1b[36m")
+        .replace(/`?\&4`?/g, "\x1b[31m")
+        .replace(/`?\&5`?/g, "\x1b[35m")
+        .replace(/`?\&6`?/g, "\x1b[33m")
+        .replace(/`?\&7`?/g, "\x1b[37m")
+        .replace(/`?\&b0`?/g, "\x1b[38m")
+        .replace(/`?\&b1`?/g, "\x1b[41m") 
+        .replace(/`?\&b2`?/g, "\x1b[42m") 
+        .replace(/`?\&b3`?/g, "\x1b[46m") 
+        .replace(/`?\&b4`?/g, "\x1b[41m") 
+        .replace(/`?\&b5`?/g, "\x1b[45m")
+        .replace(/`?\&b6`?/g, "\x1b[43m") 
+        .replace(/`?\&b7`?/g, "\x1b[47m");
+    return str + "\x1b[0m"; // plus reset 
+    }
+};
